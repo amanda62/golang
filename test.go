96c1a07165c797ae5777fa1e8e2af30bc608af2d
Amanda Smith <amanda62@ucla.edu>
2021-06-28 18:33:32 -0700

    working

---
+++ test.go
@@ -0,0 +1,17 @@
+
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+  
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
